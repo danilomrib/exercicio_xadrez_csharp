98dddb20d4aac221eb01fd87eda15939611cd73c
danilomrib <danilomrib@gmail.com>
2025-03-13 10:52:45 -0300

    Implementando o Tabuleiro (classes peça, tabuleiro, enum Cor)

---
+++ Tabuleiro/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.Tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; set; }
+        public int QtdMovimentos { get; set; }
+        public Tabuleiro Tab { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QtdMovimentos = 0;
+            Tab = tab;
+        }
+    }
+}
